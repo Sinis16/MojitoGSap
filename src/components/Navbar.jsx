0638a0cad0853d345fc5d0a0eece1088fc2c4cf6
Santiago Sinisterra <s.sinisterra@uniandes.edu.co>
2025-07-11 15:18:15 -0500

    Implementing Hero Scroll Video

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { navLinks } from '../../constants/index.js'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
-
 const Navbar = () => {
     useGSAP(() => {
         const navTween = gsap.timeline({
@@ -15,7 +14,7 @@ const Navbar = () => {
 
         navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
             backgroundColor: '#00000050',
-            backgroundFiler: 'blur(10px)',
+            backgroundFilter: 'blur(10px)',
             duration: 1,
             ease: 'power1.inOut'
         });
